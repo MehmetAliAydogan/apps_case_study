0ef236c3c5a240bd4209e9c00bb46430f5115d61
MehmetAliAydogan <mehmetaydogan1998@gmail.com>
2024-09-14 16:59:06 +0300

    change api events line endings and file paths

--- api_events.py
+++ api_events.py
@@ -6,17 +6,17 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Path to your JSON file
-json_installs = "C:/Users/mehme/Desktop/Apps_Interview/api_response/response_installs.json"
-json_events = "C:/Users/mehme/Desktop/Apps_Interview/api_response/response_events.json"
-json_cost = "C:/Users/mehme/Desktop/Apps_Interview/api_response/response_cost.json"
+json_installs = "C:/Users/mehme/Desktop/Apps_Interview/apps_case_study/api_response/response_installs.json"
+json_events = "C:/Users/mehme/Desktop/Apps_Interview/apps_case_study/api_response/response_events.json"
+json_cost = "C:/Users/mehme/Desktop/Apps_Interview/apps_case_study/api_response/response_cost.json"
 
 # Read Installs response file
 df = spark.read.option("multiLine", True).json(json_installs)
 
 
-print("Installs structure: \n")
+print("Installs structure:")
 df.printSchema()
-print("Installs data example: \n")
+print("Installs data example:")
 df.show(3)
 
 # Read Events response file
@@ -26,16 +26,16 @@ df = (spark.read
 .json(json_events))
 
 
-print("Events structure: \n")
+print("Events structure:")
 df.printSchema()
-print("Events data example: \n")
+print("Events data example:")
 df.show(3)
 
 # Read Cost response file
 df = spark.read.option("multiLine", True).json(json_cost)
 
 
-print("Cost structure: \n")
+print("Cost structure:")
 df.printSchema()
-print("Cost data example: \n")
+print("Cost data example:")
 df.show(3)
