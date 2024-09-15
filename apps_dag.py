c130e7d699941cb93fa8d379af8dd95d04a3124b
MehmetAliAydogan <mehmetaydogan1998@gmail.com>
2024-09-15 22:28:49 +0300

    update dag with filtering and aggregation operations

--- apps_dag.py
+++ apps_dag.py
@@ -57,23 +57,82 @@ def process_data():
     events_df = load_data_to_dataframe(PATH_TO_EVENTS)
     cost_df = load_data_to_dataframe(PATH_TO_COSTS)
 
-    # Example processing: filtering, aggregation, merging
-    #install_filtered = install_df[install_df['status'] == 'active']
-    #network_aggregated = network_df.groupby('network_type').agg({'bandwidth': 'mean'})
-    #cost_filtered = cost_df[cost_df['total_cost'] > 1000]
+    #Process Installs
 
-    install_df.to_csv(PATH_TO_INSTALL_PROCESSED, index=False)
-    events_df.to_csv(PATH_TO_EVENTS_PROCESSED, index=False)
+    # Convert 'installed_at' column to datetime
+    install_df['installed_at'] = pd.to_datetime(install_df['installed_at'])
+
+    # Format the installed_at as 'yyyy-mm-dd'
+    install_df['installed_at'] = install_df['installed_at'].dt.strftime('%Y-%m-%d')
+
+    # Rename the installed_at as install_date
+    install_df.rename(columns={'installed_at': 'installed_date'}, inplace=True)
+
+    #Fix network name spellings
+    install_df['network_name'] = install_df['network_name'].replace({'TikTok': 'Tiktok', 'Google': 'Google Ads', 'ironSrc' : 'IronSource'})
+
+    # Find the install count 
+    install_grouped_df = install_df.groupby(['installed_date', 'network_name', 'campaign_name']).size().reset_index(name='install_count')
+
+    #Process Events
+
+    event_name_filter = ['AdImpression', 'GameStart']
+    
+    # selecting rows based on condition 
+    events_df = events_df[events_df['event_name'].isin(event_name_filter)] 
+
+    # Convert 'event_ts' column to datetime
+    events_df['event_ts'] = pd.to_datetime(events_df['event_ts'])
+
+    # Format the event_ts as 'yyyy-mm-dd'
+    events_df['event_ts'] = events_df['event_ts'].dt.strftime('%Y-%m-%d')
+
+    # Rename the event_ts as event_date
+    events_df.rename(columns={'event_ts': 'event_date'}, inplace=True)
+
+    merged_events_df = events_df.merge(install_df, on='user_id', how='left')
+
+    # Aggregate merged_events_df table with 3 dimensions and calculate KPI's
+    # Grouping by 'install_date', 'network_name', and 'campaign_name'
+
+    grouped_df = merged_events_df.groupby(['installed_date', 'network_name', 'campaign_name']).agg(
+        # Count distinct users who sent the 'GameStart' event
+        game_start_count=('user_id', lambda x: x[merged_events_df['event_name'] == 'GameStart'].count()),
+        
+        # Sum of revenue when event_name is 'AdImpression' and ad_type is 'banner'
+        banner_revenue=('ad_revenue', lambda x: x[(merged_events_df['event_name'] == 'AdImpression') & (merged_events_df['ad_type'] == 'banner')].sum()),
+        
+        # Sum of revenue when event_name is 'AdImpression' and ad_type is 'rewarded'
+        interstitial_revenue=('ad_revenue', lambda x: x[(merged_events_df['event_name'] == 'AdImpression') & (merged_events_df['ad_type'] == 'interstitial')].sum()),
+
+        # Sum of revenue when event_name is 'AdImpression' and ad_type is 'rewarded'
+        rewarded_revenue=('ad_revenue', lambda x: x[(merged_events_df['event_name'] == 'AdImpression') & (merged_events_df['ad_type'] == 'rewarded')].sum())
+    ).reset_index()
+
+    #Process Cost 
+    # Convert 'date' column to datetime
+    cost_df['date'] = pd.to_datetime(cost_df['date'])
+    cost_df['date'] = cost_df['date'].dt.strftime('%Y-%m-%d')
+
+    # Aggregate cost table with 3 dimensions and sum the Costs
+    cost_df = cost_df.groupby(['date', 'network_name', 'campaign_name'])['cost'].sum().reset_index()
+
+
+    install_grouped_df.to_csv(PATH_TO_INSTALL_PROCESSED, index=False)
+    grouped_df.to_csv(PATH_TO_EVENTS_PROCESSED, index=False)
     cost_df.to_csv(PATH_TO_COSTS_PROCESSED, index=False)
 
 
 def create_final_table():
     # Load the JSON files into DataFrames
-    install_df = pd.read_csv(PATH_TO_INSTALL_PROCESSED)
-    events_df = pd.read_csv(PATH_TO_EVENTS_PROCESSED)
+    install_grouped_df = pd.read_csv(PATH_TO_INSTALL_PROCESSED)
+    grouped_df = pd.read_csv(PATH_TO_EVENTS_PROCESSED)
     cost_df = pd.read_csv(PATH_TO_COSTS_PROCESSED)
 
-    final_table = install_df
+    final_df = grouped_df.merge(cost_df, left_on=['installed_date', 'network_name', 'campaign_name'], right_on=['date', 'network_name', 'campaign_name'], how='left')
+
+    final_table = final_df[['installed_date', 'network_name', 'campaign_name', 'cost', 'game_start_count', 'banner_revenue', 'interstitial_revenue', 'rewarded_revenue']]
+    final_table['cost'] = final_table['cost'].fillna(0)
     
     # Save the final table to CSV
     final_table.to_csv('/output/final_table.csv', index=False)
@@ -106,7 +165,8 @@ fetch_network_data = PythonOperator(
 fetch_cost_data = PythonOperator(
     task_id='fetch_cost_data',
     python_callable=fetch_data_from_api,
-    op_kwargs={'api_url': 'https://dataeng-interview-project-zomet35f7q-uc.a.run.app/network_cost?cost_date={}'.format(datetime.today().strftime('%Y-%m-%d')), 'file_name': PATH_TO_COSTS},
+    op_kwargs={'api_url': 'https://dataeng-interview-project-zomet35f7q-uc.a.run.app/network_cost?cost_date={}'
+               .format((datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')), 'file_name': PATH_TO_COSTS},
     dag=dag
 )
 
